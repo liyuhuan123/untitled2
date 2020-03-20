c4102c42d931db77329cef1d3d4a37b4a6ea048c
liyuhuan123 <2502268134@qq.com>
2020-03-20 14:37:42 +0800

    0320代码练习

--- src/Java0319/Main.java
+++ src/Java0319/Main.java
@@ -1,9 +1,13 @@
 package Java0319;
-import java.util.Date;
+import static java.lang.Math.*;
 public class Main {
     public static void main(String[] args){
-      Date date  = new Date();
-      //得到一个毫秒级别的时间戳
-        System.out.println(date.getTime());
+        double x = 30;
+        double y = 40;
+        //静态导入的方式写起来更方便一些
+        double result = Math.sqrt(Math.pow(x,2)+Math.pow(y,2));
+        double result1 = sqrt(pow(x,2)+pow(y,2));
+        System.out.println(result);
+        System.out.println(result1);
     }
 }
