fba576ce52ae40e131dd59a380de94aaae19ecc1
liyuhuan123 <2502268134@qq.com>
2020-03-25 15:59:43 +0800

    0325代码练习

--- src/Java0319/Java0322/Demo.java
+++ src/Java0319/Java0322/Demo.java
@@ -1,30 +1,30 @@
 package Java0319.Java0322;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Demo {
     public static void main(String[] args){
-        Collection<String> list = new ArrayList<String>();
-        System.out.println(list.size());
-        System.out.println(list.isEmpty());
-        list.add("我");
-        list.add("爱");
-        list.add("java");
-        System.out.println(list.size());
-        System.out.println(list.isEmpty());
-        Object[] array = list.toArray();
-        System.out.println(Arrays.toString(array));
-        for(String s : list){
-            System.out.println(s);
+        Map<String,String> map = new HashMap();
+        System.out.println(map.size());
+        System.out.println(map.isEmpty());
+        System.out.println(map.get("作者"));
+        //System.out.println(map.getOrDefault("作者","艺名"));
+        System.out.println(map.containsKey("作者"));
+        System.out.println(map.containsValue("艺名"));
+        map.put("作者","鲁迅");
+        map.put("标题","狂人日记");
+        map.put("发表时间","1918年");
+        System.out.println(map.size());
+        System.out.println(map.isEmpty());
+        System.out.println(map.get("作者"));
+        //System.out.println(map.getOrDefault("作者","艺名"));
+        System.out.println(map.containsKey("作者"));
+        System.out.println(map.containsValue("艺名"));
+        for(Map.Entry<String,String>entry : map.entrySet()){
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
         }
-        list.remove("爱");
-        for(String s : list){
-            System.out.println(s);
-        }
-        list.clear();
-        System.out.println(list.size());
-        System.out.println(list.isEmpty());
     }
+
 }
