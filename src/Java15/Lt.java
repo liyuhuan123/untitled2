b783599e5f072b681278434cf36146b6158a924f
liyuhuan123 <2502268134@qq.com>
2020-03-30 17:47:00 +0800

    0330代码练习

--- src/Java15/Lt.java
+++ src/Java15/Lt.java
@@ -1,33 +1,31 @@
 package Java15;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.Stack;
 
-class Employee{
-    public int id;
-    public int importance;
-    public List<Integer> subordinates;
-}
 public class Lt {
-    public int getImportance(List<Employee> employees, int id) {
-        if(employees.size() == 0){
-            return 0;
+    //输入两个整数序列，
+    //第一个序列表示栈的压入顺序，
+    //请判断第二个序列是否可能为该栈的弹出顺序。
+    //假设压入栈的所有数字均不相等。例如序列1,2,3,4,5是某栈的压入顺序，
+    //序列4,5,3,2,1是该压栈序列对应的一个弹出序列，
+    //但4,3,5,1,2就不可能是该压栈序列的弹出序列。
+      //      （注意：这两个序列的长度是相等的）
+    public boolean IsPopOrder(int [] pushA,int [] popA) {
+        if(pushA.length == 0 ||
+                popA.length == 0 ||
+                pushA.length != popA.length){
+            return false;
         }
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(id);
-        int sum = 0;
-        while(!(queue.size() == 0)){
-            int tmp = queue.poll();
-            for(Employee e : employees){
-                if(tmp == e.id){
-                    sum += e.importance;
-                    for(int i : e.subordinates){
-                        queue.add(i);
-                    }
-                }
+        int j = 0;
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0;i < pushA.length;i++){
+            stack.push(pushA[i]);
+            while(!stack.isEmpty() && stack.peek() == popA[j]){
+                j++;
+                stack.pop();
             }
         }
-        return sum;
+        return stack.isEmpty();
     }
 }
+
