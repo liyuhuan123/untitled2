ce45ffb86a8f11d5f446bbdea9b3c873df714936
liyuhuan123 <2502268134@qq.com>
2020-04-01 15:47:50 +0800

    0401代码练习

--- src/Java15/Interview.java
+++ src/Java15/Interview.java
@@ -1,117 +1,44 @@
 package Java15;
 
+
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
-
-class TreeNode{
-    public int val;
-    public TreeNode left;
-    public TreeNode right;
-
-    public TreeNode(int val) {
-        this.val = val;
-    }
-}
 
 public class Interview {
-    public boolean isSameTree(TreeNode p,TreeNode q){
-        if(p == null && q == null){
-            return true;
-        }
-        if(p == null || q == null){
-            return false;
-        }
-        if(p.val != q.val){
-            return false;
-        }
-        return isSameTree(p.left,q.left) &&
-                isSameTree(p.right,q.right);
-    }
-        public boolean isSubtree(TreeNode s, TreeNode t) {
-//给定两个非空二叉树 s 和 t，
-// 检验 s 中是否包含和 t 具有相同结构和节点值的子树
-// s 的一个子树包括 s 的一个节点和这个节点的所有子孙。
-// s 也可以看做它自身的一棵子树。
-         if(s == null && t == null){
-             return true;
-         }
-         if(s == null || t == null){
-             return false;
-         }
-         boolean ret = false;
-         if(s.val == t.val){
-             ret = isSameTree(s,t);
-         }
-         return ret||isSubtree(s.left,t)||isSubtree(s.right,t);
-        }
-    public int maxDepth(TreeNode root) {
-//给定一个二叉树，找出其最大深度。
-//二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
-//说明: 叶子节点是指没有子节点的节点。
-        if(root == null){
-            return 0;
-        }
-        int depth = 0;
-        if(root.left == null &&root.right == null){
-            return 1;
-        }
-        int leftDepth = maxDepth(root.left);
-        int rightDepth = maxDepth(root.right);
-        return 1 + (Math.max(leftDepth,rightDepth));
-    }
-    public boolean isBalanced(TreeNode root) {
-//给定一个二叉树，判断它是否是高度平衡的二叉树。
-//本题中，一棵高度平衡二叉树定义为：
-//一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
-        if(root == null){
-            return true;
-        }
-        if(root.left == null || root.right == null){
-            return true;
-        }
-        int leftDepth = maxDepth(root.left);
-        int rightDepth = maxDepth(root.right);
-        if(leftDepth - rightDepth > 1 || leftDepth-rightDepth < -1 ){
-            return false;
-        }
-        return isBalanced(root.left) && isBalanced(root.right);
-    }
-    public boolean isSymmetric(TreeNode root) {
-//给定一个二叉树，检查它是否是镜像对称的。
-//例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
-        if(root == null){
-            return true;
-        }
-        return isMirror(root.left,root.right);
-    }
-    private boolean isMirror(TreeNode s,TreeNode t){
-        if(s == null && t == null){
-            return true;
-        }
-        if(s == null || t == null){
-            return false;
-        }
-        if(s.val != t.val){
-            return false;
-        }
-        return isMirror(s.left,t.right) && isMirror(s.right,t.left);
-    }
-    //二叉树的层序遍历
-    public void levelOrder(TreeNode root){
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        while(!queue.isEmpty()){
-            TreeNode cur = queue.poll();
-            System.out.println(cur.val+" ");
-            if(cur.left != null){
-                queue.offer(cur.left);
-            }
-            if(cur.right != null){
-                queue.offer(cur.right);
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+//给你一个 m 行 n 列的二维网格 grid 和一个整数 k。
+// 你需要将 grid 迁移 k 次
+//每次「迁移」操作将会引发下述活动：
+//位于 grid[i][j] 的元素将会移动到 grid[i][j + 1]。
+//位于 grid[i][n - 1] 的元素将会移动到 grid[i + 1][0]。
+//位于 grid[m - 1][n - 1] 的元素将会移动到 grid[0][0]。
+//请你返回 k 次迁移操作后最终得到的 二维网格
+        List<List<Integer>> listlist = new ArrayList<>();
+        List<Integer> list1 = new ArrayList<>();
+        //获取行数
+        int rowLength = grid.length;
+        //获取列数
+        int colLength = grid[0].length;
+        for (int i = 0; i < rowLength; i++) {
+            for (int j = 0; j < colLength; j++) {
+                list1.add(grid[i][j]);
             }
         }
-    }
+        List<Integer> list2 = new ArrayList<>();
+        int total = rowLength * colLength;
+        int prev = total - k;
+        int count = 0;
+        while(count < total){
 
+            list2.add(list1.get(prev % total));
+            prev++;
+            count++;
+            if(count % colLength == 0){
+                listlist.add(((List)new ArrayList<>(list1).clone()));
+                list1.clear();
+            }
+
+        }
+        return listlist;
     }
+}
